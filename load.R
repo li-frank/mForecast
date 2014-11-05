c41637402f2f686eac5ce27db9312e5b44f1cd87
unknown <frankli@D-SJC-00552087.corp.ebay.com>
2014-11-05 12:33:19 -0800

    go

--- load.R
+++ load.R
@@ -1,4 +1,10 @@
 #load data
 SQltable <- 'p_csi_tbs_t.fl_mGMBforecast_v1'
 
+c <- teradataConnect()
+sqlPath <- 'C:/Users/frankli/Dropbox (eBayMob&Eng)/FrankL/Rproj/appVersGMB/GMB_appVers_Date.sql'
+
+sqlQuery <- paste(readLines(sqlPath), collapse=" ")
+df <- dbGetQuery(c,sqlQuery)
+df2 <- df
 ##autorefresh numbers
