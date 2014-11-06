8172e33266ce43c8e2a5381b3b9488d5a83c3810
unknown <frankli@D-SJC-00552087.corp.ebay.com>
2014-11-06 10:10:19 -0800

    go

--- update.R
+++ update.R
@@ -22,7 +22,7 @@ removed <- dbSendQuery(c,removeQuery)
 ##reload
 reloadPath <- 'C:/Users/frankli/Dropbox (eBayMob&Eng)/FrankL/Forecast/ForecastModel/mForecast/dly_mGMB_s1_reload.sql'
 reloadQuery <- paste(readLines(reloadPath), collapse=" ")
-reloadQuery <- gsub(':start_dt',reloadStart,reloadQuery); reloadQuery
+reloadQuery <- gsub(':start_dt',reloadStart,reloadQuery)
 reloadQuery <- gsub(':end_dt',reloadEnd,reloadQuery); reloadQuery
 reloaded <- dbSendQuery(c,reloadQuery)
 
@@ -33,6 +33,13 @@ minDate
 maxDate
 days
 
-#check
-sumDate <- ddply(df,.(created_dt),summarize,gmb=sum(gmb_plan))
-plot(sumDate$created_dt,sumDate$gmb)
+#check last 365 days
+#sumDate <- ddply(df,.(created_dt),summarize,gmb=sum(gmb_plan))
+byDate <- group_by(df,created_dt)
+sumDate <- summarise(byDate,gmb=sum(gmb_plan)); head(sumDate)
+
+#filter
+sumDate <- sumDate[sumDate$created_dt>=max(sumDate$created_dt) - 368,]; sumDate
+sumDate <- filter(sumDate, created_dt >= max(sumDate$created_dt) - 368); sumDate
+
+plot_sumDate <- ggplot(sumDate,aes(x=created_dt,y=gmb)) + geom_line(); plot_sumDate
