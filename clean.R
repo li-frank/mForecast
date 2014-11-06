663de70958de291198f0cb2dd961becdc7d0a607
unknown <frankli@D-SJC-00552087.corp.ebay.com>
2014-11-06 15:26:13 -0800

    go

--- clean.R
+++ clean.R
@@ -1,28 +1,25 @@
 #create DFs for mobile, mobileAgg, PC
 
-#gmb over time
-##old: sumDate <- ddply(df,.(created_dt),summarize,gmb=sum(gmb_plan))
+##gmb over time
+###old: sumDate <- ddply(df,.(created_dt),summarize,gmb=sum(gmb_plan))
 df.gbDate <- group_by(df,created_dt)
 df.Date <- summarise(df.gbDate,gmb=sum(gmb_plan)); head(df.Date)
 #filter for last 2 years
 df.Date_2yr <- df.Date[df.Date$created_dt>=max(df.Date$created_dt) - (366*2+2),]; df.Date_2yr
 
-#mobile gmb by platform over time
+##mobile gmb by platform over time
 mobile <- df[df$platform != 'Core Site on PC',]
-mobile.gbDatePlat <- group_by(mobile[mobile$created_dt>=max(mobile$created_dt)-366,],created_dt, platform)
+mobile.gbDatePlat <- group_by(mobile,created_dt, platform)
 mobile.DatePlat <- summarise(mobile.gbDatePlat,gmb=sum(gmb_plan)); head(mobile.DatePlat)
 
-#mobile aggregate
-byDate <- group_by(mobile[mobile$created_dt>=max(mobile$created_dt)-366,],created_dt, platform)
-sumDatePlat <- summarise(byDatePlat,gmb=sum(gmb_plan)); head(sumDatePlat)
-plot_platGMB <- ggplot(sumDatePlat,
-                       aes(x=created_dt,y=gmb, color=platform)) + geom_line(); plot_platGMB
+##mobile aggregate
+mobile.gbDate <- group_by(mobile,created_dt)
+mobile.Date <- summarise(mobile.gbDate,gmb=sum(gmb_plan)); head(mobile.gbDatePlat)
 
 
-
-
-PC <- df[df$platform == 'Core Site on PC',]
-byDatePlat <- group_by(PC[PC$created_dt>=max(PC$created_dt)-366,],created_dt)
+##pc
+pc <- df[df$platform == 'Core Site on PC',]
+pc.gbDatePlat <- group_by(pc[pc$created_dt>=max(pc$created_dt)-366,],created_dt)
 sumDatePC <- summarise(byDatePlat,gmb=sum(gmb_plan)); head(sumDatePlat)
 plot_platGMB <- ggplot(sumDatePlat,
                        aes(x=created_dt,y=gmb, color=platform)) + geom_line(); plot_platGMB
--- plots.R
+++ plots.R
@@ -1,13 +1,15 @@
 #plot gmb over time
 plot_df.Date <- ggplot(df.Date,
                        aes(x=created_dt,y=gmb, color=format(created_dt,"%Y"))) + geom_line(); plot_df.Date
-
-#plot mobile gmb by platform over time
-plot_mobile.DatePlat <- ggplot(mobile.DatePlat,
-                               aes(x=created_dt,y=gmb, color=platform)) + geom_line(); plot_mobile.DatePlat
-
 #plot gmb over time (rolling 2 years)
 Year <- format(df.Date_2yr$created_dt,"%Y")
 monthDate <- format(df.Date_2yr$created_dt,"%m-%d")
 plot_df.Date_2yr <- ggplot(df.Date_2yr,
                            aes(x=monthDate,y=gmb, color=Year)) + geom_line(aes(group=Year)); plot_df.Date_2yr
+
+#plot mobile gmb by platform over time
+plot_mobile.DatePlat <- ggplot(mobile.DatePlat,
+                               aes(x=created_dt,y=gmb, color=platform)) + geom_line(); plot_mobile.DatePlat
+#plot mobileAgg gmb over time
+plot_mobile.Date <- ggplot(mobile.Date,
+                       aes(x=created_dt,y=gmb)) + geom_line(); plot_mobile.Date
