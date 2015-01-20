95499f48fe10ab67c62a14b1077e8baa75cdbe26
Frank Li <frankli2@ebay.com>
2015-01-19 22:55:18 -0800

    go

--- deleteDates.R
+++
@@ -1,6 +0,0 @@
-##remove
-removePath <- 'C:/Users/frankli/Dropbox (eBayMob&Eng)/FrankL/Forecast/ForecastModel/mForecast/SQL/dly_mGMB_remove.sql'
-removeQuery <- paste(readLines(removePath), collapse=" ")
-removeQuery <- gsub(':start_dt',reloadStart,removeQuery); removeQuery
-removeQuery <- gsub(':table',actualTable,removeQuery); removeQuery
-removed <- dbSendQuery(c,removeQuery)
